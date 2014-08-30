d11133cc2457baea941b26314ff82dfb93b5d7ba
Mbechezi Mlanawo <mlanawo.mbechezi@ikimea.com>
2014-08-30 18:45:55 +0200

    init project

---
+++ src/Gitcolab/Bundle/AppBundle/GitcolabAppBundle.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * This file is part of Gitcolab.
+ *
+ * (c) Mbechezi mlanawo <mlanawo@mbechezi.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Gitcolab\Bundle\AppBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class GitcolabAppBundle extends Bundle
+{
+
+}
