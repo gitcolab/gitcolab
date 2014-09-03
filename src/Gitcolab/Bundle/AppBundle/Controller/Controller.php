45911f0ac2593ae81dca5ef8400ab0b9dc3e6f3c
Mbechezi Mlanawo <mlanawo.mbechezi@ikimea.com>
2014-09-04 00:12:38 +0200

    add shortcut

--- src/Gitcolab/Bundle/AppBundle/Controller/Controller.php
+++ src/Gitcolab/Bundle/AppBundle/Controller/Controller.php
@@ -13,7 +13,55 @@ namespace Gitcolab\Bundle\AppBundle\Controller;
 
 use FOS\RestBundle\Controller\FOSRestController;
 
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\Query;
+use Pagerfanta\Pagerfanta;
+use Pagerfanta\Adapter\DoctrineORMAdapter;
+
 class Controller extends FOSRestController
 {
+    /**
+     * Returns the paginator instance configured for the given query and page
+     * number
+     *
+     * @param  Query   $query The query
+     * @param  integer $page  The current page number
+     * @param  integer $limit Results per page
+     *
+     * @return Pagerfanta
+     */
+    protected function getPaginator(Query $query, $page, $limit = 10)
+    {
+        $paginator = new Pagerfanta(new DoctrineORMAdapter($query));
+        $paginator
+            ->setMaxPerPage($limit)
+            ->setCurrentPage($page, false, true)
+        ;
+
+        return $paginator;
+    }
+
+    /**
+     * @param $class
+     * @param string $domaine
+     * @return \Doctrine\Common\Persistence\ObjectRepository
+     */
+    protected function getRepository($class, $domaine = 'GitcolabAppBundle')
+    {
+        return $this->getDoctrine()->getRepository($domaine. ':'.$class);
+    }
+
+    /**
+     * @param $entity
+     * @param bool $flush
+     */
+    protected function persistAndFlush($entity, $flush = true)
+    {
+        $this->getDoctrine()->getManager()->persist($entity);
+
+        if ($flush) {
+            $this->getDoctrine()->getManager()->flush();
+        }
+    }
 
 }
