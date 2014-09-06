ea6d65c2f3e61f65dda6972d54c4977eeae362c5
Mbechezi Mlanawo <mlanawo.mbechezi@ikimea.com>
2014-09-06 02:27:39 +0200

    fix CS

--- src/Gitcolab/Bundle/AppBundle/Model/Key.php
+++ src/Gitcolab/Bundle/AppBundle/Model/Key.php
@@ -13,13 +13,40 @@ namespace Gitcolab\Bundle\AppBundle\Model;
 
 class Key
 {
+    /**
+     * @var int
+     */
     protected $id;
+
+    /**
+     * @var string
+     */
     protected $key;
+
+    /**
+     * @var string
+     */
     protected $title;
+
+    /**
+     * @var object
+     */
     protected $user;
+
+    /**
+     * @var object
+     */
     protected $createdAt;
+
+    /**
+     * @var object
+     */
     protected $updatedAt;
 
+    /**
+     * @param $key
+     * @return $this
+     */
     public function setKey($key)
     {
         $this->key = $key;
@@ -27,11 +54,18 @@ class Key
         return $this;
     }
 
+    /**
+     * @return string
+     */
     public function getKey()
     {
         return $this->key;
     }
 
+    /**
+     * @param $title
+     * @return $this
+     */
     public function setTitle($title)
     {
         $this->title = $title;
@@ -39,32 +73,56 @@ class Key
         return $this;
     }
 
+    /**
+     * @return string
+     */
     public function getTitle()
     {
         return $this->title;
     }
 
-    public function setCreatedAt($createdAt)
+    /**
+     * @param \DateTime $createdAt
+     * @return $this
+     */
+    public function setCreatedAt(\DateTime $createdAt)
     {
         $this->createdAt = $createdAt;
 
         return $this;
     }
+
+    /**
+     * @return mixed
+     */
     public function getCreatedAt()
     {
         return $this->createdAt;
     }
-    public function setUpdatedAt($updatedAt)
+
+    /**
+     * @param \DateTime $updatedAt
+     * @return $this
+     */
+    public function setUpdatedAt(\DateTime $updatedAt)
     {
         $this->updatedAt = $updatedAt;
 
         return $this;
     }
+
+    /**
+     * @return \DateTime
+     */
     public function getUpdatedAt()
     {
         return $this->updatedAt;
     }
 
+    /**
+     * @param User $user
+     * @return $this
+     */
     public function setUser(User $user)
     {
         $this->user = $user;
@@ -72,6 +130,9 @@ class Key
         return $this;
     }
 
+    /**
+     * @return User
+     */
     public function getUser()
     {
         return $this->user;
