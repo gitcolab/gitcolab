268a2f40b21c58b617126ed220c229f4e77bf396
Mlanawo Mbechezi <mlanawo.mbechezi@ikimea.com>
2015-04-26 03:16:58 +0200

    reduce controller

--- src/Gitcolab/Bundle/AppBundle/Controller/User/SecurityController.php
+++ src/Gitcolab/Bundle/AppBundle/Controller/User/SecurityController.php
@@ -25,26 +25,11 @@ class SecurityController extends Controller
      */
     public function loginAction(Request $request)
     {
-        $session = $request->getSession();
-        
-        // get the login error if there is one
-        if ($request->attributes->has(SecurityContextInterface::AUTHENTICATION_ERROR)) {
-            $error = $request->attributes->get(
-                SecurityContextInterface::AUTHENTICATION_ERROR
-            );
-        } elseif (null !== $session && $session->has(SecurityContextInterface::AUTHENTICATION_ERROR)) {
-            $error = $session->get(SecurityContextInterface::AUTHENTICATION_ERROR);
-            $session->remove(SecurityContextInterface::AUTHENTICATION_ERROR);
-        } else {
-            $error = '';
-        }
-
-        // last username entered by the user
-        $lastUsername = (null === $session) ? '' : $session->get(SecurityContextInterface::LAST_USERNAME);
+        $helper = $this->get('security.authentication_utils');
 
         return $this->render('GitcolabAppBundle:User:login.html.twig', [
-            'error' => $error,
-            'last_username' => $lastUsername
+            'last_username' => $helper->getLastUsername(),
+            'error'         => $helper->getLastAuthenticationError(),
         ]);
     }
 }
